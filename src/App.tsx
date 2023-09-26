2e13489c98a1da0f885dc0db1d9bea4a9b36e4da
samsolichin <samsolichin01@gmail.com>
2023-09-27 00:13:23 +1000

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import { ThemeProvider } from "@/components/theme-provider";
+import { ModeToggle } from '@/components/mode-toggle';
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0);
 
   return (
-    <>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <ModeToggle></ModeToggle>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className="grid w-full max-w-sm items-center gap-1.5">
+        <Label htmlFor="decimal-number">Decimal Number</Label>
+        <Input type="numeric" id="decimal-number" placeholder="Decimal Number" />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <div className="grid w-full max-w-sm items-center gap-1.5">
+        <Label htmlFor="binary-number">Binary Number</Label>
+        <Input type="string" id="binary-number" placeholder="Binary Number" />
+      </div>
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
